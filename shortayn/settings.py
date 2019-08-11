b6ef7cf9b8cd958fa4a593367df7540ea6e932ed
elseagle <tay2druh@gmail.com>
2019-08-11 16:34:58 +0100

    whitenoise issue fixed

--- shortayn/settings.py
+++ shortayn/settings.py
@@ -139,7 +139,7 @@ DATABASES['default'].update(db_from_env)
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
